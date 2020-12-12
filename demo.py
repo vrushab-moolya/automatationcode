7e9c56f56c853eafb813cae92524665984326b3c
vrushab-moolya <vrushab.patil@moolya.com>
2020-12-12 20:42:46 +0530

    second commit

--- demo.py
+++ demo.py
@@ -9,7 +9,7 @@ driver.find_element(By.NAME, 'user-name').send_keys('standard_user')
 driver.find_element(By.NAME, 'password').send_keys('secret_sauce')
 driver.find_element(By.ID, 'login-button').click()
 
-driver.find_element(By.XPATH,'(//div[@class="inventory_item_name"])[1]').click()
+driver.find_element(By.XPATH,'(//div[@class="inventory_item_name"])[3]').click()
 
 time.sleep(2)
 cart=driver.find_element(By.XPATH,'//button[@class="btn_primary btn_inventory"]')
@@ -22,8 +22,8 @@ if product.text== 'Sauce Labs Backpack' :
 
 driver.find_element(By.XPATH,"//a[.='CHECKOUT']").click()
 
-driver.find_element(By.ID,'first-name').send_keys('manasa')
-driver.find_element(By.ID,'last-name').send_keys('m')
+driver.find_element(By.ID,'first-name').send_keys('vrushab')
+driver.find_element(By.ID,'last-name').send_keys('moolya')
 driver.find_element(By.ID,'postal-code').send_keys('590004')
 driver.find_element(By.XPATH,"//input[@type='submit']").click()
 
